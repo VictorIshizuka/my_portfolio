20aeb01a91b533e6307fd2078514e70abc5748b4
Victor Rosario Ishizuka <victorishizuka131@gmail.com>
2024-08-01 16:09:51 -0300

    feat: created initial data and banner

---
+++ src/components/Banner/index.tsx
@@ -0,0 +1,32 @@
+import styles from "./Banner.module.css";
+import coloredCircle from "../../assets/circulo_colorido.png";
+import myImage from "../../assets/circulo_colorido.png";
+
+export function Banner() {
+  return (
+    <div className={styles.banner}>
+      <div className={styles.apresentation}>
+        <h1 className={styles.title}>Bem vindo!</h1>
+
+        <p className={styles.paragraph}>
+          Eu sou Victor Ishizuka, desenvolvedor forntend júnior. Aqui
+          compartilho vários conhecimentos, espero que aprenda algo novo :)
+        </p>
+      </div>
+
+      <div className={styles.images}>
+        <img
+          className={styles.coloredCircle}
+          src={coloredCircle}
+          aria-hidden={true}
+        />
+
+        <img
+          className={styles.myImage}
+          src={myImage}
+          alt="Foto do Antônio Evaldo sorrindo"
+        />
+      </div>
+    </div>
+  );
+}
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -3,11 +3,13 @@ import { Home } from "../pages/Home";
 import { SobreMim } from "../pages/SobreMim";
 import { NotFound } from "../pages/NotFound";
 import { MenuLink } from "../components/MenuLink";
+import { Banner } from "../components/Banner";
 
 export const ComponentRoutes = (): JSX.Element => {
   return (
     <BrowserRouter>
       <MenuLink />
+      <Banner />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/sobre-mim" element={<SobreMim />} />
